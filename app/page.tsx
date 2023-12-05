f07431d7c13c561b6bf30aef1ac963450474dcc1
KpryKorn <roffinisacha@gmail.com>
2023-12-05 16:37:49 +0100

    add tests to display prisma db

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>Hello World!</div>;
+import { getAllUsers } from "@/lib/script";
+
+export default async function Home() {
+  const users = await getAllUsers();
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.id}: {user.username}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
