4ebea0df28253808926f5129ddaae8d10f7063d6
KpryKorn <roffinisacha@gmail.com>
2023-12-12 10:20:31 +0100

    style: Update user list display format

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ export default async function Home() {
   const users = await getAllUsers();
   return (
     <div>
-      <h1>Users</h1>
+      <h1 className="text-2xl font-semibold mb-4">Users</h1>
       <ul>
         {users &&
           users.map((user) => (
             <li key={user.id}>
-              {user.id}: {user.username}
+              {user.id} — {user.username}
             </li>
           ))}
       </ul>
