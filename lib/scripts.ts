45bada6a955726757007521e723ae12bde0434ba
KpryKorn <roffinisacha@gmail.com>
2023-12-12 14:08:44 +0100

    feat: Add createUser function to scripts.ts

--- lib/scripts.ts
+++ lib/scripts.ts
@@ -1,9 +1,23 @@
+import { TData } from "@/types/Types";
 import prisma from "./prisma";
 
 export async function getAllUsers() {
   try {
     const users = await prisma.users.findMany();
     return users;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function createUser(data: TData) {
+  try {
+    const user = await prisma.users.create({
+      data,
+    });
+    console.log(user);
   } catch (error) {
     console.error(error);
   } finally {
