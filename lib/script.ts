ab4d37111f5abed462b1070fd912a1aeb787e7be
KpryKorn <roffinisacha@gmail.com>
2023-12-12 10:02:59 +0100

    refactor: getAllUsers function to use try-catch-finally block

--- lib/script.ts
+++ lib/script.ts
@@ -1,16 +1,12 @@
 import prisma from "./prisma";
 
-export async function getAllUsers() {
-  const users = await prisma.users.findMany();
-  console.log(users);
-  return users;
+export default async function getAllUsers() {
+  try {
+    const users = await prisma.users.findMany();
+    return users;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
-
-getAllUsers() // TODO: besoin d'appeler 2x ?? tout mettre dans une seule fonction
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-  });
