937d4290247fbaa50e0cb232002f22ffdc08cc23
KpryKorn <roffinisacha@gmail.com>
2023-12-12 10:02:15 +0100

    style: adjusted padding to fit 100dvh

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={inter.className + " container bg-slate-950 text-white"}>
-        <main className="my-6 md:my-12">{children}</main>
+        <main className="py-6 md:py-12">{children}</main>
       </body>
     </html>
   );
