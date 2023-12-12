b5f7c60efc8ccf38fa66d0f6f237568ef52e142f
KpryKorn <roffinisacha@gmail.com>
2023-12-12 15:37:16 +0100

    feat: client component w/ server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/button";
 import { getAllUsers } from "@/lib/actions";
 
 export default async function Home() {
@@ -13,6 +14,7 @@ export default async function Home() {
             </li>
           ))}
       </ul>
+      <Button />
     </div>
   );
 }
