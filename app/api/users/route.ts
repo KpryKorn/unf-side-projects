2ed0d3800428e0d8a2ca0ef9cc368b7a1bc07890
KpryKorn <roffinisacha@gmail.com>
2023-12-13 15:57:03 +0100

    feat: add PUT route for updating user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -8,3 +8,12 @@ export async function POST(req: Request) {
   });
   return NextResponse.json(user);
 }
+
+export async function PUT(req: Request) {
+  const body = await req.json();
+  const user = await prisma.users.update({
+    where: { id: body.id },
+    data: body,
+  });
+  return NextResponse.json(user);
+}
