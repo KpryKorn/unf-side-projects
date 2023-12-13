95e69ffbdf469a4b7f137c3fbaf0ab570c5ab592
KpryKorn <roffinisacha@gmail.com>
2023-12-13 15:57:20 +0100

    style: updated layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,14 @@
-import ProfileForm from "@/components/Form/ProfileForm";
+import ProfileForm from "@/app/dashboard/ProfileForm";
 import Link from "next/link";
 
 export default async function Dashboard() {
   return (
     <>
       <h1>Dashboard</h1>
-      <ProfileForm />
-      <Link href="/" className="rounded-lg bg-blue-400 p-4">
+      <Link href="/" className="mt-4 rounded-lg bg-blue-400 p-4 inline-block">
         Home
       </Link>
+      <ProfileForm />
     </>
   );
 }
