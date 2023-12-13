c38a3a363aee356c864abfd650ccda2ddac66c4a
KpryKorn <roffinisacha@gmail.com>
2023-12-13 16:00:50 +0100

    style: update layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ export default async function Home() {
   return (
     <div>
       <h1 className="text-2xl font-semibold mb-8">Users</h1>
-      <Link href="/users" className="rounded-lg bg-blue-400 p-4">
+      <Link href="/users" className="rounded-lg bg-blue-400 p-4 mr-4">
         Users
+      </Link>
+      <Link href="/dashboard" className="rounded-lg bg-blue-400 p-4 mr-4">
+        Dashboard
       </Link>
     </div>
   );
