84b3a62ea363c004392f1773cf562fa4a9b581d9
KpryKorn <roffinisacha@gmail.com>
2023-12-13 10:16:48 +0100

    feat: add loading state to fetching UI

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex space-x-3 animate-pulse">
+      <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+      <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+      <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+    </div>
+  );
+}
