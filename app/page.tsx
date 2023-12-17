9fabbb78675a6ee1d1eddb2871d0787e7ecfe079
KpryKorn <roffinisacha@gmail.com>
2023-12-17 10:50:57 +0100

    style: add all types of buttons styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,56 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-semibold mb-8">Users</h1>
-      <Link href="/users" className="btn btn-small">
-        Users
-      </Link>
-      <Link href="/users" className="btn">
-        Users
-      </Link>
-      <Link href="/users" className="btn btn-large">
-        Users
-      </Link>
-      <Link href="/dashboard" className="btn btn-large btn-primary">
-        Dashboard
-      </Link>
+      <div className="flex gap-4">
+        <div className="flex flex-col items-start gap-2">
+          <span className="btn btn-small">Users</span>
+          <span className="btn">Users</span>
+          <span className="btn btn-large">Users</span>
+        </div>
+        <div className="flex flex-col items-start gap-2">
+          <span className="btn btn-small">Dashboard</span>
+          <span className="btn">Dashboard</span>
+          <span className="btn btn-large">Dashboard</span>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <div className="flex flex-col items-start gap-2">
+          <span className="btn btn-primary btn-small">Users</span>
+          <span className="btn btn-primary">Users</span>
+          <span className="btn btn-primary btn-large">Users</span>
+        </div>
+        <div className="flex flex-col items-start gap-2">
+          <span className="btn btn-primary btn-small">Dashboard</span>
+          <span className="btn btn-primary">Dashboard</span>
+          <span className="btn btn-primary btn-large">Dashboard</span>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <div className="flex flex-col items-start gap-2">
+          <span className="btn btn-success btn-small">Users</span>
+          <span className="btn btn-success">Users</span>
+          <span className="btn btn-success btn-large">Users</span>
+        </div>
+        <div className="flex flex-col items-start gap-2">
+          <span className="btn btn-success btn-small">Dashboard</span>
+          <span className="btn btn-success">Dashboard</span>
+          <span className="btn btn-success btn-large">Dashboard</span>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <div className="flex flex-col items-start gap-2">
+          <span className="btn btn-critical btn-small">Users</span>
+          <span className="btn btn-critical">Users</span>
+          <span className="btn btn-critical btn-large">Users</span>
+        </div>
+        <div className="flex flex-col items-start gap-2">
+          <span className="btn btn-critical btn-small">Dashboard</span>
+          <span className="btn btn-critical">Dashboard</span>
+          <span className="btn btn-critical btn-large">Dashboard</span>
+        </div>
+      </div>
     </div>
   );
 }
