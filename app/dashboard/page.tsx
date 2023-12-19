9375efcabea2eb8c223460a24d58157450ef1df1
KpryKorn <roffinisacha@gmail.com>
2023-12-19 15:58:52 +0100

    now showcasing all posts on this page to test instant refresh on submit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,10 @@
 import ProfileForm from "./ProfileForm";
 import Link from "next/link";
 import PostForm from "./PostForm";
+import prisma from "@/lib/prisma";
 
 export default async function Dashboard() {
+  const posts = await prisma.post.findMany();
   return (
     <>
       <h1>Dashboard</h1>
@@ -11,6 +13,17 @@ export default async function Dashboard() {
       </Link>
       <ProfileForm />
       <PostForm />
+      <div>
+        {posts.map((post) => {
+          return (
+            <div key={post.id} className="my-8 p-2">
+              <h2 className="text-2xl font-semibold">{post.title}</h2>
+              <p>{post.content}</p>
+              <time>{post.createdAt.toDateString()}</time>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
