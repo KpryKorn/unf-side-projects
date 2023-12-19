a231bf66f8272cea31982316979002b0e73d5295
KpryKorn <roffinisacha@gmail.com>
2023-12-19 15:58:04 +0100

    Update loading component to display "Loading..."

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div>
-      <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-gray-300"></div>
+      <h1>Loading...</h1>
     </div>
   );
 }
