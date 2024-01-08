9a5ee37b0bdf41b9abaaf7a502204964f710521d
KpryKorn <roffinisacha@gmail.com>
2024-01-08 16:43:50 +0100

    feat: add user profile page and styling

--- app/(user)/users/[id]/page.tsx
+++ app/(user)/users/[id]/page.tsx
@@ -2,6 +2,10 @@ import prisma from "@/lib/prisma";
 import Image from "next/image";
 import defaultPfp from "@/assets/default-profile-picture.jpeg";
 import { Metadata } from "next";
+import Chip from "@/components/chip";
+import FeaturedProject from "@/components/FeaturedProject";
+import { MessageButton, ParamButton } from "@/components/buttons";
+import { ExperienceProfileBadge, ProjectsProfileBadge } from "./profile-badges";
 
 interface UserProfileProps {
   params: {
@@ -45,31 +49,43 @@ export default async function UserProfile({ params }: UserProfileProps) {
   const { name, bio, image, role, age } = user ?? {};
 
   return (
-    <div>
-      <h1>{name}</h1>
+    <>
+      <div className="absolute -z-10 top-0 left-0 w-full h-[275px] bg-gradient-to-r from-orange-200 to-purple-400"></div>
+      <article className="mt-[-20px]">
+        <div className="flex items-end justify-between mb-8">
+          <Image
+            src={image ?? defaultPfp}
+            alt={name ?? "Profile picture"}
+            width={200}
+            height={200}
+            className="rounded-full p-1  border border-white bg-white shadow"
+          />
+          <div className="flex gap-1">
+            <ParamButton />
+            <MessageButton />
+          </div>
+        </div>
 
-      <Image
-        src={image ?? defaultPfp}
-        alt={name ?? "Profile picture"}
-        width={200}
-        height={200}
-        className="rounded-full"
-      />
+        <div className="flex items-center justify-between mt-[10px] mb-1">
+          <div className="flex flex-col items-start gap-2">
+            <div className="flex items-center gap-2">
+              <h1 className="font-medium text-3xl leading-none tracking-tight">
+                {name ?? "Incomplete name"}
+              </h1>
+              <Chip text={role} />
+            </div>
+            <p>{bio ?? ""}</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <ProjectsProfileBadge projects={39} />
+            <ExperienceProfileBadge experience={2} />
+          </div>
+        </div>
 
-      <div className="mt-[10px] mb-1">
-        <h2>Bio:</h2>
-        <p>{bio ?? "Incomplete biography"}</p>
-      </div>
+        <hr className="my-8" />
 
-      <div className="mt-[10px] mb-1 flex items-center gap-1">
-        <h2>Role:</h2>
-        <p>{role}</p>
-      </div>
-
-      <div className="mt-[10px] mb-1 flex items-center gap-1">
-        <h2>Age:</h2>
-        <p>{age ?? "Incomplete"}</p>
-      </div>
-    </div>
+        <FeaturedProject />
+      </article>
+    </>
   );
 }
